69d86ae76b030efc8cf30521f8fac3a70148321a
DrakeDong0 <109976775+DrakeDong0@users.noreply.github.com>
2023-07-17 02:28:31 +0000

    Update 1 added grapes and strawberries

--- Plants/Grape.java
+++ Plants/Grape.java
@@ -5,7 +5,7 @@ It inherits properties and methods from the Plant class and adds additional func
 */
 //import package
 package Plants;
- 
+
 import processing.core.PApplet;
 import processing.core.PImage;
 
@@ -13,7 +13,7 @@ import processing.core.PImage;
 public class Grape extends Plant {
   public static int limit = 6; // The maximum growth limit for Grape plants
   boolean growing = true; // Indicates if the grape is currently growing
-  boolean watered = false; //Indicates if the crop is watered
+  boolean watered = false; // Indicates if the crop is watered
 
   /**
    * 
@@ -25,9 +25,9 @@ public class Grape extends Plant {
    * @param startdate The start date of the grape
    */
   public Grape(PApplet p, int x, int y, int startdate) {
-    //super's the parent class's variables
+    // super's the parent class's variables
     super(p, x, y, startdate);
-    //sets the grape images
+    // sets the grape images
     this.image = app.loadImage("PlantPictures/grape1.png");
     this.waterimage = app.loadImage("PlantPictures/water.png");
   }
@@ -39,7 +39,7 @@ public class Grape extends Plant {
    * @return A string containing information about the grape plant
    */
   public String getInfo() {
-    //returns crop data
+    // returns crop data
     String str1 = Integer.toString(startdate);
     return "This crop was grown on day " + str1;
   }
@@ -53,48 +53,53 @@ public class Grape extends Plant {
   public boolean cropCheck() {
     return true;
   }
-   /**
+
+  /**
    * 
    * Checks if the grape is mature
+   * 
    * @return true if the crop is ready to be harvested
    */
-  public boolean matureCheck(){
-    if (growing == false){
+  public boolean matureCheck() {
+    if (growing == false) {
       return true;
-    }
-    else{
+    } else {
       return false;
-      }
+    }
   }
- /**
+
+  /**
    * 
    * Waters the plant
    * 
    */
-  public void water(int daynum){
+  public void water(int daynum) {
     watered = true;
   }
+
   /**
    * 
    * Draws the grape plant on the screen.
    */
   public void draw() {
-    app.image(image, x+10, y+10);
-    //draws the water icon if crop isn't watered
-    if (watered == false){
-      app.image(waterimage,x+10,y+10);
+    app.image(image, x + 10, y + 10);
+    // draws the water icon if crop isn't watered
+    if (watered == false) {
+      app.image(waterimage, x + 10, y + 10);
     }
   }
+
   /**
    * 
    * Harvests the crop
    */
-  public void harvest(){
-    //harvests the crop if it has stopped growing. Replaces it with a dirt picture
-    if (growing == false){
+  public void harvest() {
+    // harvests the crop if it has stopped growing. Replaces it with a dirt picture
+    if (growing == false) {
       this.image = app.loadImage("Pictures/dirt.png");
     }
   }
+
   /**
    * 
    * Grows the grape plant based on the given day number.
@@ -102,13 +107,14 @@ public class Grape extends Plant {
    * @param daynum The current day number
    */
   public void grow(int daynum) {
-    //daynumx is the number of days the crop has been alive for
+    // daynumx is the number of days the crop has been alive for
     int daynumx = daynum - startdate;
-    //number is modded by 5 to get the remainder. This is used to calculate which growth stage it is on
+    // number is modded by 5 to get the remainder. This is used to calculate which
+    // growth stage it is on
     int n = daynumx % 6;
-    //only grows if it is watered and new
+    // only grows if it is watered and new
     if (growing && watered) {
-      //Each case displays a different growth stage image
+      // Each case displays a different growth stage image
       if (n == 0) {
         this.image = app.loadImage("PlantPictures/grape1.png");
       }
@@ -121,11 +127,12 @@ public class Grape extends Plant {
       if (n == 3) {
         this.image = app.loadImage("PlantPictures/grape4.png");
       }
-      if (n == 4){
+      if (n == 4) {
         this.image = app.loadImage("PlantPictures/grape5.png");
       }
       if (n == 5) {
-        //once it has reached the final stage, the image stops changing and growing is turned false
+        // once it has reached the final stage, the image stops changing and growing is
+        // turned false
         this.image = app.loadImage("PlantPictures/grape0.png");
         growing = false;
       }
